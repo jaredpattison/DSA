514a9078a2f5337dd1e3eb27a8b6e85d6fe8b779
jared <jaredpattison@users.noreply.github.com>
2019-08-21 10:58:22 -0700

    selection sort

---
+++ warmups/dsaPractice/lib/practice-sorting-start.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const numbers = [2, 4, 6, 1, 7, 19, 12, 65, 43, 77, 65, 100, 3];
+
+const selectionSort = (array, compare) => {
+
+};
+
+const mergeSort = (items, compare) => {
+
+};
+
+const quickSort = (items, compare, left, right) => {
+
+};
+
+console.log(selectionSort(numbers));
+console.log(mergeSort(numbers));
+console.log(quickSort(numbers));
